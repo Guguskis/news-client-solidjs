eacb20cb04a01d37491ca58b0afda4f3e6d8df93
Guguskis <matasliutikas@gmail.com>
2022-05-16 20:07:26 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import { createSignal, createEffect, onCleanup } from "solid-js";
+import { createStore } from "solid-js/store";
+
+function createLocalStore(initState) {
+  const [state, setState] = createStore(initState);
+  if (localStorage.todos) setState(JSON.parse(localStorage.todos));
+  // createEffect(() => (localStorage.todos = JSON.stringify(state)));
+  return [state, setState];
+}
+
+function App() {
+  const [store, setStore] = createLocalStore({
+    counter: 0,
+    users: ["Sally", "Matt"],
+  });
+
+  onCleanup(() => {
+    console.log("STOP")
+  })
+
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid ${store.counter}
+        </a>
+        <button
+          onClick={() => {
+            setStore({ counter: store.counter + 1 });
+            console.log(store.counter);
+            // setStore(store => {
+            //   store.counter + 1;
+            //   return store;
+            // });
+          }}
+        >
+          HI
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
