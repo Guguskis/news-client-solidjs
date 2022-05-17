bf0ce52f7935605c0f3640a79cc3fe67e1ae8fcd
Guguskis <matasliutikas@gmail.com>
2022-05-17 19:41:34 +0300

    refactor

--- src/hooks/useNewsClient.jsx
+++ src/hooks/useNewsClient.jsx
@@ -5,19 +5,18 @@ function useNewsClient() {
   const [loading, setLoading] = createSignal(false);
   const [nextPageToken, setNextPageToken] = createSignal(0);
 
-  const [newsResource, { refetch }] = createResource(async () => {
+  const [response, { refetch }] = createResource(async () => {
     setLoading(true);
     const response = await fetch(
-      `http://localhost:9081/api/news?pageToken=${nextPageToken()}&pageSize=10`
+      `http://86.100.240.140:9081/api/news?pageToken=${nextPageToken()}&pageSize=10`
     );
-    const body = await response.json();
-    setNextPageToken(body.nextPageToken);
-    return body.news;
+    return await response.json();
   });
 
   createEffect(() => {
-    if (newsResource()) {
-      setNews((news) => news.concat(newsResource()));
+    if (response()) {
+      setNews(uniqueByIdMerger(response().news));
+      setNextPageToken(response().nextPageToken);
       setLoading(false);
     }
   });
@@ -40,3 +39,13 @@ function useNewsClient() {
 }
 
 export default useNewsClient;
+
+function uniqueByIdMerger(newItems) {
+  return (array) => {
+    return array.concat(
+      newItems.filter((n) => {
+        return !array.find((a) => a.id === n.id);
+      })
+    );
+  };
+}
