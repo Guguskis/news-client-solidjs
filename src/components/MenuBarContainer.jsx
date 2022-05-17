fa8d4b3dc488b28a2d1fffa896f715d65e44a672
Guguskis <matasliutikas@gmail.com>
2022-05-17 18:02:33 +0300

    formatting

--- src/components/MenuBarContainer.jsx
+++ src/components/MenuBarContainer.jsx
@@ -1,13 +1,13 @@
-import Container from "@suid/material/Container"
+import Container from "@suid/material/Container";
 import MenuBar from "./MenuBar.jsx";
 
 function MenuBarContainer(params) {
-    return (
-        <Container sx={{ pt: 10 }}>
-            <MenuBar />
-            {params.children}
-        </Container>
-    );
+  return (
+    <Container sx={{ pt: 0 }}>
+      <MenuBar />
+      {params.children}
+    </Container>
+  );
 }
 
 export default MenuBarContainer;
