e3c2abc879d7bc86f67ec656f15ab6509c1771b2
Guguskis <matasliutikas@gmail.com>
2022-05-21 15:37:26 +0300

    formatting

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
   },
   server: {
     port: 9080,
-    host: '0.0.0.0',
-  }
+    host: "0.0.0.0",
+  },
 });
