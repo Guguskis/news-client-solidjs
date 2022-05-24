f46671270f89f71d35a54b95fd55d64577d74644
Guguskis <matasliutikas@gmail.com>
2022-05-25 00:09:11 +0300

    added page loading spinners

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { ThemeProvider } from "@suid/material/styles";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
 import { Route, Routes } from "solid-app-router";
-import { lazy } from "solid-js";
+import { lazy, Suspense } from "solid-js";
+import CenteredCircularProgress from "./components/CenteredCircularProgress.jsx";
 import theme from "./config/theme.jsx";
 import { NewsProvider } from "./hooks/useNews.jsx";
 
@@ -27,9 +28,30 @@ function App() {
           <CssBaseline />
           <MenuBar hideOnScroll={true} />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/subreddits" element={<Subreddits />} />
-            <Route path="*" element={<NotFound />} />
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<CenteredCircularProgress />}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/subreddits"
+              element={
+                <Suspense fallback={<CenteredCircularProgress />}>
+                  <Subreddits />
+                </Suspense>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <Suspense fallback={<CenteredCircularProgress />}>
+                  <NotFound />
+                </Suspense>
+              }
+            />
           </Routes>
           {/* <ToastContainer /> */}
         </ThemeProvider>
---
+++ src/components/CenteredCircularProgress.jsx
@@ -0,0 +1,20 @@
+import CircularProgress from "@suid/material/CircularProgress";
+import Grid from "@suid/material/Grid";
+
+function CenteredCircularProgress() {
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="center"
+      sx={{ height: "100vh" }}
+    >
+      <Grid item sx={{ margin: "auto" }}>
+        <CircularProgress />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default CenteredCircularProgress;
