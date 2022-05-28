177bb83badcf080217730ee0a8eae088751e97a1
Guguskis <matasliutikas@gmail.com>
2022-05-28 17:23:15 +0300

    table uses news data

--- src/pages/Dataset.jsx
+++ src/pages/Dataset.jsx
@@ -9,18 +9,6 @@ import TableRow from "@suid/material/TableRow";
 import Typography from "@suid/material/Typography";
 import { useNews } from "../hooks/useNews";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function Dataset() {
   const { news } = useNews();
 
@@ -31,25 +19,19 @@ export default function Dataset() {
         <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Subreddit</TableCell>
+              <TableCell align="right">Title</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {news().map((row) => (
               <TableRow
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                r/{row.subChannel}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right" sx={{maxWidth: "100px"}}>{row.title}</TableCell>
               </TableRow>
             ))}
           </TableBody>
