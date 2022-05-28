2f74eacfd110cb6fc9a49ff4ddb395fddfdd769d
Guguskis <matasliutikas@gmail.com>
2022-05-28 17:43:11 +0300

    extracted table row

--- src/pages/Dataset.jsx
+++ src/pages/Dataset.jsx
@@ -1,5 +1,7 @@
+import Button from "@suid/material/Button";
 import Container from "@suid/material/Container";
 import Paper from "@suid/material/Paper";
+import Stack from "@suid/material/Stack";
 import Table from "@suid/material/Table";
 import TableBody from "@suid/material/TableBody";
 import TableCell from "@suid/material/TableCell";
@@ -7,14 +9,37 @@ import TableContainer from "@suid/material/TableContainer";
 import TableHead from "@suid/material/TableHead";
 import TableRow from "@suid/material/TableRow";
 import Typography from "@suid/material/Typography";
+import { createEffect } from "solid-js";
 import { useNews } from "../hooks/useNews";
 
+function NewsTableCell({ news }) {
+  return (
+    <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+      <TableCell component="th" scope="row">
+        r/{news.subChannel}
+      </TableCell>
+      <TableCell align="right" sx={{ maxWidth: "100px" }}>
+        {news.title}
+      </TableCell>
+    </TableRow>
+  );
+}
+
 export default function Dataset() {
-  const { news } = useNews();
+  const { news, loadMore } = useNews();
+
+  createEffect(() => {
+    console.log(news());
+  });
 
   return (
     <Container>
       <Typography variant="h4">News dataset</Typography>
+      <Stack direction="row" sx={{ paddingBottom: 1, paddingTop: 1 }}>
+        <Button component="p" variant="contained" onClick={loadMore}>
+          Load more
+        </Button>
+      </Stack>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
@@ -24,15 +49,8 @@ export default function Dataset() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {news().map((row) => (
-              <TableRow
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                r/{row.subChannel}
-                </TableCell>
-                <TableCell align="right" sx={{maxWidth: "100px"}}>{row.title}</TableCell>
-              </TableRow>
+            {news().map((news) => (
+              <NewsTableCell news={news} />
             ))}
           </TableBody>
         </Table>
