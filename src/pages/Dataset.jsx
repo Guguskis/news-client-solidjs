9792074ab6b36966a586953557f82791dc307a7f
Guguskis <matasliutikas@gmail.com>
2022-05-28 18:23:48 +0300

    disabled overflow for text

--- src/pages/Dataset.jsx
+++ src/pages/Dataset.jsx
@@ -9,10 +9,21 @@ import TableContainer from "@suid/material/TableContainer";
 import TableHead from "@suid/material/TableHead";
 import TableRow from "@suid/material/TableRow";
 import Typography from "@suid/material/Typography";
+import styled from "@suid/system/styled";
 import { createEffect } from "solid-js";
 import { useNews } from "../hooks/useNews";
 import { uniqueByIdMerger } from "../util/merger";
 import { createLocalSignal } from "../util/util";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  // whiteSpace: "nowrap",
+  // textOverflow: "ellipsis",
+  // width: "600px",
+  // display: "block",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  maxWidth: theme.spacing(30),
+}));
 
 function enchanceDatasetRow(item) {
   return {
@@ -22,7 +33,7 @@ function enchanceDatasetRow(item) {
   };
 }
 
-function TableHeader() {
+function HeaderRow() {
   return (
     <TableHead>
       <TableRow>
@@ -35,13 +46,15 @@ function TableHeader() {
   );
 }
 
-function NewsTableCell({ news }) {
+function NewsTableRow({ news }) {
   return (
     <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
       <TableCell component="th" scope="row">
         r/{news.subChannel}
       </TableCell>
-      <TableCell sx={{ maxWidth: "150px" }}>{news.title}</TableCell>
+      <StyledTableCell>
+        {news.title}
+      </StyledTableCell>
       <TableCell>{news.category}</TableCell>
       <TableCell>{news.isPredictionCorrect ? "Yes" : "No"}</TableCell>
     </TableRow>
@@ -64,8 +77,10 @@ export default function Dataset() {
 
   return (
     <Container>
-      <Typography variant="h3" gutterBottom >Dataset</Typography>
-      <Stack direction="row" spacing={1} sx={{ paddingBottom: 1}} >
+      <Typography variant="h3" gutterBottom>
+        Dataset
+      </Typography>
+      <Stack direction="row" spacing={1} sx={{ paddingBottom: 1 }}>
         <Button component="p" variant="contained" onClick={loadMore}>
           Load more
         </Button>
@@ -75,10 +90,10 @@ export default function Dataset() {
       </Stack>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} size="small">
-          <TableHeader />
+          <HeaderRow />
           <TableBody>
             {dataset().map((news) => (
-              <NewsTableCell news={news} />
+              <NewsTableRow news={news} />
             ))}
           </TableBody>
         </Table>
